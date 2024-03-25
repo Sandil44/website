45af443f40e3f3651f5f1d14f1df772844b0c53e
Sandil44 <162865635+Sandil44@users.noreply.github.com>
2024-03-25 23:39:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Page Title -->
+    <title>SG Labs Management</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/x-icon">
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/linearicons.css">
+    <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="assets/css/style2.css">
+</head>
+<body>
+    <!-- Preloader Starts -->
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- Preloader End -->
+
+<!-- Navbar Start -->
+<nav class="navbar justify-content-center navbar-light bg-primary">
+    <
+    <ul class="nav">
+    <li class="nav-item">
+        <a class="nav-link active" href="#">Home</a>
+    </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Sign Up
+          </a>
+          <ul class="dropdown-menu nav-link drp-dwn">
+            <li><a class="dropdown-item" href="backend\admin\index.php">Admin</a></li>
+            <li><a class="dropdown-item" href="backend/doc/index.php">Doctor</a></li>
+            <li><a class="dropdown-item" href="backend/SIGNUP/index.php">Patient</a></li>
+          </ul>
+        </li>
+    <li class="nav-item">
+        <a class="nav-link" href="home/appointment.html ">Appointment</a>
+    </li>
+    </ul>
+    
+</nav>
+<!-- Navbar End -->
+
+
+
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area">
+        <div class="container background-wht">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h4>Caring for better life</h4>
+                    <h1>Leading the way in medical excellence</h1>
+                    <p>SG Labs is awarded as one of the Top Hospital Management System, which can integrate all the HIS systems, processes and machines into an intelligent information system to derive operational efficiency and assist hospitals in decision making process through MIS and Analytics.</p>
+                </div>
+
+                <div class="col-lg-5">
+                    <img class="" src="../images/doctor.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Javascript -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="assets/js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="assets/js/vendor/wow.min.js"></script>
+    <script src="assets/js/vendor/owl-carousel.min.js"></script>
+    <script src="assets/js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="assets/js/vendor/jquery.nice-select.min.js"></script>
+    <script src="assets/js/vendor/superfish.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+   
+</body>
+</html>
